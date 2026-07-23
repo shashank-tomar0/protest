e361db4fa796ac1d1899d7d6924134539a09efca
shashank-tomar0 <shashanktomar912@gmail.com>
2026-07-23 09:32:04 +0530

    fix: switch to HashRouter to fix React Error #62

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
-import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom'
-import { useEffect, useState, useRef } from 'react'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Movement from './pages/Movement'
 import Timeline from './pages/Timeline'
@@ -7,34 +6,10 @@ import TakeAction from './pages/TakeAction'
 import Donate from './pages/Donate'
 import Contact from './pages/Contact'
 
-function AnimatedPage({ children }) {
-  const location = useLocation()
-  const [display, setDisplay] = useState(children)
-  const [stage, setStage] = useState('animate')
-  const prev = useRef(location.pathname)
-
-  useEffect(() => {
-    if (location.pathname !== prev.current) {
-      setStage('initial')
-      const t = setTimeout(() => {
-        setDisplay(children)
-        setStage('animate')
-        prev.current = location.pathname
-      }, 200)
-      return () => clearTimeout(t)
-    } else {
-      setDisplay(children)
-    }
-  }, [location.pathname])
-
-  return <div style={{ opacity: stage === 'animate' ? 1 : 0, transition: 'opacity 0.3s' }}>{display}</div>
-}
-
-function AppRoutes() {
-  const location = useLocation()
+export default function App() {
   return (
-    <AnimatedPage>
-      <Routes location={location} key={location.pathname}>
+    <HashRouter>
+      <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/movement" element={<Movement />} />
         <Route path="/timeline" element={<Timeline />} />
@@ -42,14 +17,6 @@ function AppRoutes() {
         <Route path="/donate" element={<Donate />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
-    </AnimatedPage>
-  )
-}
-
-export default function App() {
-  return (
-    <BrowserRouter>
-      <AppRoutes />
-    </BrowserRouter>
+    </HashRouter>
   )
 }
